6b2155dc69702b0fbd48cac5a6094467163ffe9e
Lee Kwang Min <minwinn1@gmail.com>
2026-02-22 00:30:45 +0900

    상품검색 및 페이지네이션

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,5 +1,6 @@
 const Product = require("../models/Product");
 
+const PAGE_SIZE=1;
 const productController = {}
 productController.createProduct=async(req,res)=>{
     try{
@@ -35,8 +36,26 @@ productController.createProduct=async(req,res)=>{
 
 productController.getProducts = async(req,res) => {
     try{
-        const products = await Product.find({});
-        res.status(200).json({status:"success",data:products});
+        const {page, name} = req.query;
+        const cond = name?{name:{$regex:name,$options:'i'}}:{};
+        let query = Product.find(cond);
+        let response = {status:"success"};
+
+        if(page){
+            query.skip((page-1)*PAGE_SIZE).limit(PAGE_SIZE);
+            // 최종 몇개 페이지
+            // 데이터가 총 몇개 있는지
+            const totalItemNum = await Product.find(cond).countDocuments();  // count()는 이전버전임, 오류 -> countDocuments() 로 변경함
+            // 데이터 총 개수 / PAGE_SIZE
+            const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+            response.totalPageNum = totalPageNum;
+        
+        }
+
+        const productList = await query.exec();
+        response.data = productList;
+      //res.status(200).json({response});   // 강사 코드로 오류 ->  response를 그대로 보낸다
+        res.status(200).json(response);
     }catch(error) {
         res.status(400).json({status:"fail",error:error.message});
     }
