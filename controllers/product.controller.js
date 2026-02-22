c6632cd50f02d50f147fc755893f814855b5a85a
Lee Kwang Min <minwinn1@gmail.com>
2026-02-23 01:00:42 +0900

    상품 수정하기

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,9 +1,10 @@
 const Product = require("../models/Product");
 
-const PAGE_SIZE=1;
+const PAGE_SIZE = 1;
 const productController = {}
-productController.createProduct=async(req,res)=>{
-    try{
+
+productController.createProduct = async (req, res) => {
+    try {
         const {
             sku,
             name,
@@ -28,36 +29,63 @@ productController.createProduct=async(req,res)=>{
         });
 
         await product.save();
-        res.status(200).json({status:"success", product});
-    }catch(error){
-        res.status(400).json({status:"fail", error: error.message});
-    }    
+        res.status(200).json({ status: "success", product });
+    } catch (error) {
+        res.status(400).json({ status: "fail", error: error.message });
+    }
 };
 
-productController.getProducts = async(req,res) => {
-    try{
-        const {page, name} = req.query;
-        const cond = name?{name:{$regex:name,$options:'i'}}:{};
+productController.getProducts = async (req, res) => {
+    try {
+        const { page, name } = req.query;
+        const cond = name ? { name: { $regex: name, $options: 'i' } } : {};
         let query = Product.find(cond);
-        let response = {status:"success"};
+        let response = { status: "success" };
 
-        if(page){
-            query.skip((page-1)*PAGE_SIZE).limit(PAGE_SIZE);
+        if (page) {
+            query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
             // 최종 몇개 페이지
             // 데이터가 총 몇개 있는지
             const totalItemNum = await Product.find(cond).countDocuments();  // count()는 이전버전임, 오류 -> countDocuments() 로 변경함
             // 데이터 총 개수 / PAGE_SIZE
             const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
             response.totalPageNum = totalPageNum;
-        
+
         }
 
         const productList = await query.exec();
         response.data = productList;
-      //res.status(200).json({response});   // 강사 코드로 오류 ->  response를 그대로 보낸다
+        //res.status(200).json({response});   // 강사 코드로 오류 ->  response를 그대로 보낸다
         res.status(200).json(response);
-    }catch(error) {
-        res.status(400).json({status:"fail",error:error.message});
+    } catch (error) {
+        res.status(400).json({ status: "fail", error: error.message });
+    }
+};
+
+productController.updateProduct = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const {
+          sku, 
+          name, 
+          size, 
+          image, 
+          price, 
+          description, 
+          category, 
+          stock, 
+          status,
+        } = req.body;
+
+        const product = await Product.findByIdAndUpdate(
+            {_id: productId},
+            {sku, name, size, image, price, description, category, stock, status},
+            {new : true}   // 상품 수정하기, 업데이트한 후 새로운 값을 반환받을 수 있다.
+        );
+        if(!product) throw new Error("item doesn't exist");
+        res.status(200).json({status:"success", data: product});
+    } catch (error) {
+        res.status(400).json({ status: "fail", error: error.message });
     }
 };
 
